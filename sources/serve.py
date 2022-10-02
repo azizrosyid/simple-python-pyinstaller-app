70a51de1d4ff85ea0f1af408a719e8214611a53b
Aziz Rosyid <azizrosyid783@gmail.com>
2022-10-02 18:14:14 +0700

    edit jenkinsfile

--- sources/serve.py
+++ sources/serve.py
@@ -4,6 +4,11 @@ import socketserver
 PORT = 3000
 DIR = "sources/dist"
 
+# list all files
+import os
+files = os.listdir(DIR)
+print("Files in directory: " + str(files))
+
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIR, **kwargs)
