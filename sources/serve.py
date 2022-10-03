39ae1b31d0fa0a9579b97d33d802d0c4de433ecb
Aziz Rosyid <azizrosyid783@gmail.com>
2022-10-03 23:29:19 +0700

    edit jenkinsfile

---
+++ sources/serve.py
@@ -0,0 +1,13 @@
+import http.server
+import socketserver
+
+PORT = 3000
+DIR = "."
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIR, **kwargs)
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Server started at localhost:" + str(PORT))
+    httpd.serve_forever()
