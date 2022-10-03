79f6547d28dd0c6a2eff4587850942dc3d9c17d6
Aziz Rosyid <azizrosyid783@gmail.com>
2022-10-03 23:32:52 +0700

    edit jenkinsfile

--- sources/serve.py
+++ sources/serve.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 
 PORT = 3000
-DIR = "."
+DIR = "./sources/dist"
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
